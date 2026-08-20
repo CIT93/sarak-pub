d1bfb8d13a30b05c415411bed84e06581e0d7ff5
AntsEverywhere <skeovilai1@my.scccd.edu>
2026-08-20 01:46:08 +0000

    Step 2.1 Complete

--- w2/app.js
+++ w2/app.js
@@ -2,8 +2,8 @@ console.log('Hello from app.js! Your JavaScript is connected and running!');
 // --- Part 1: Select HTML Elements ----
 // We use document.getElementById() to get a reference to an element by its unique ID.
 // We store these references in 'const' variables because the elements themselves won't change.
-const messageDisplayElement = document.getElementById("output-message");
-const updateButton = document.getElementById("update-button");
+const totalDisplayElement = document.getElementById("total-display");
+const addItemButton = document.getElementById("add-item-btn");
 
 //These variables will change as the user interacts with the page.
 let userName = 'Sara';
@@ -25,13 +25,13 @@ const handleButtonClick = function() {
         // We can even change the style of an HTML element directly with JavaScript!
         // Change text color
         message += ' WOW, you are a super clicker!';
-        messageDisplayElement.style.color = 'purple';
+        totalDisplayElement.style.color = 'purple';
     } else {
-        messageDisplayElement.style.color = '#333';
+        totalDisplayElement.style.color = '#333';
     }
     // Update the text content of our paragraph element on the page.
     // This is how JavaScript makes changes visible on the web page!
-    messageDisplayElement.textContent = message;
+    totalDisplayElement.textContent = message;
 
     console.log(`Button Clicked! Current click count: ${clickCount}`);
 };
@@ -43,9 +43,9 @@ document.addEventListener('DOMContentLoaded', function(){
     // The 'DOMContentLoaded' event is perfect for this. It fires when the HTML document is ready.
 
     console.log(`DOM fully loaded and parsed, App is ready for interaction`);
-    // Attach an event listener to our 'updateButton.'
-    // When 'updateButton' receives a 'click' event, the 'handleButtonClick' function will execute.
-    updateButton.addEventListener('click', handleButtonClick);
+    // Attach an event listener to variable addItemButton
+    // When addItemButton receives a 'click' event, the 'handleButtonClick' function will execute.
+    addItemButton.addEventListener('click', handleButtonClick);
 
-    messageDisplayElement.textContent = `Welcome, ${userName}! Click the button below to start counting.`;
+    totalDisplayElement.textContent = `Welcome, ${userName}! Click the button below to start counting.`;
 });
