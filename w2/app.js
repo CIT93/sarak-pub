bde85d7579e9a68bbc4224b623f301550ba8f721
AntsEverywhere <skeovilai1@my.scccd.edu>
2026-08-20 02:34:28 +0000

    Step 2.3 Complete

--- w2/app.js
+++ w2/app.js
@@ -1,6 +1,6 @@
 console.log('Hello from app.js! Your JavaScript is connected and running!');
-// --- Part 1: Select HTML Elements ----
-// We use document.getElementById() to get a reference to an element by its unique ID.
+
+// We use document.getElementById() to get a reference to an element by its unique ID in HTML file
 // We store these references in 'const' variables because the elements themselves won't change.
 const totalDisplayElement = document.getElementById("total-display");
 const addItemButton = document.getElementById("add-item-btn");
@@ -8,28 +8,23 @@ const addItemButton = document.getElementById("add-item-btn");
 // 'let' variables change, 'const' do not
 const itemPrice = 15;
 let totalCost = 0;
-// console.log(`The item price is ${itemPrice}. Your total cost is currently ${totalCost}.`);
 
-// function to record effect of a click
+// function to record effect of a click (adds itemPrice to totalCost)
 const handleButtonClick = function() {
-    totalCost += 1;
-    // Template strings (literal) to easily combine our variables and text into one message
-    let message = `Hello! You have clicked the button ${totalCost} time(s).`;
+    totalCost += itemPrice;
+    let message = `Current total: $${totalCost}`;
 
     // 'if' 'else' function to change message + color based on totalCost
-    if(totalCost >= 5) {
-        // We can even change the style of an HTML element directly with JavaScript!
-        // Change text color
-        message += ' WOW, you are a super clicker!';
-        totalDisplayElement.style.color = 'purple';
+    if(totalCost >= 60) {
+        message += ' (Over budget!)';
+        totalDisplayElement.style.color = 'red';
     } else {
-        totalDisplayElement.style.color = '#333';
+        totalDisplayElement.style.color = 'black';
     }
-    // Update the text content of our paragraph element on the page.
-    // This is how JavaScript makes changes visible on the web page!
+
     totalDisplayElement.textContent = message;
 
-    console.log(`Button Clicked! Current click count: ${totalCost}`);
+    console.log(`Item added! Total cost: ${totalCost}`);
 };
 
 
