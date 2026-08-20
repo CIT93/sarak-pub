2b9398ff51a0a32eabdc0a87f67507f000a606d0
AntsEverywhere <skeovilai1@my.scccd.edu>
2026-08-20 02:58:00 +0000

    Step 2.4 Complete

--- w2/app.js
+++ w2/app.js
@@ -12,14 +12,14 @@ let totalCost = 0;
 // function to record effect of a click (adds itemPrice to totalCost)
 const handleButtonClick = function() {
     totalCost += itemPrice;
-    let message = `Current total: $${totalCost}`;
+    let message = `Current Total: $${totalCost}`;
 
     // 'if' 'else' function to change message + color based on totalCost
     if(totalCost >= 60) {
         message += ' (Over budget!)';
         totalDisplayElement.style.color = 'red';
     } else {
-        totalDisplayElement.style.color = 'black';
+        totalDisplayElement.style.color = 'green';
     }
 
     totalDisplayElement.textContent = message;
@@ -29,14 +29,14 @@ const handleButtonClick = function() {
 
 
 document.addEventListener('DOMContentLoaded', function(){
-    // --- Part 3: Make the Button Clickable (Event Listener) ---
     // This part ensures our JavaScript code runs only AFTER the HTML is fully loaded and parsed.
     // The 'DOMContentLoaded' event is perfect for this. It fires when the HTML document is ready.
 
     console.log(`DOM fully loaded and parsed, App is ready for interaction`);
-    // Attach an event listener to variable addItemButton
+    
+    // eventListener means when event happens, function occurs
     // When addItemButton receives a 'click' event, the 'handleButtonClick' function will execute.
     addItemButton.addEventListener('click', handleButtonClick);
 
-    totalDisplayElement.textContent = `Welcome! Click the button below to start counting.`;
+    totalDisplayElement.textContent = `Welcome! Click the button below to add items and start shopping.`;
 });
